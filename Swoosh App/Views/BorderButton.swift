af4a85fef01ca6c8a2abbe50e99f1dadd8c0da7d
James Brown <jamezbrown100@gmail.com>
2019-06-14 17:59:06 -0500

    Create border button

---
+++ Swoosh App/Views/BorderButton.swift
@@ -0,0 +1,19 @@
+//
+//  BorderButton.swift
+//  Swoosh App
+//
+//  Created by James Brown on 6/14/19.
+//  Copyright © 2019 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    
+    }
+
+}
