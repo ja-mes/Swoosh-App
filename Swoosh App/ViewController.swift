7f54ac640b2efd1d1ad871d790babaa92e5cdf25
James Brown <jamezbrown100@gmail.com>
2019-06-14 18:48:39 -0500

    create new vc

--- Swoosh App/ViewController.swift
+++ Swoosh App/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var BGImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
