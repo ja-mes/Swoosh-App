f5593f1596457c5c79abfeae36aa943b6e3dbe5d
James Brown <jamezbrown100@gmail.com>
2019-06-15 11:58:06 -0500

    pass data between vc

---
+++ Swoosh App/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh App
+//
+//  Created by James Brown on 6/15/19.
+//  Copyright © 2019 James Brown. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String?
+    
+}
