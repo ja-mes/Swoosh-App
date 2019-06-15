6d0636782d54d655cbb73ac9db2faa09c7a4c674
James Brown <jamezbrown100@gmail.com>
2019-06-15 10:57:52 -0500

    new vc and segue

--- Swoosh App/WelcomeVC.swift
+++ Swoosh App/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var BGImg: UIImageView!
     
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
-        
+         
     }
 
 
